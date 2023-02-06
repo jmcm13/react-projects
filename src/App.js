4923ce5b4c9e527f37eaba4c4aaacf4336544caa
jmcm <joshuamichalmundo@gmail.com>
2023-02-06 18:17:18 +0800

    updated base code

--- src/App.js
+++ src/App.js
@@ -1,34 +1,11 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-  state = [
-    {
-      name: "Joshua",
-      age: 25
-    },
-    {
-      name: "Michal",
-      age: 25
-    },
-    {
-      name: "Mundo",
-      age: 25
-    }
-  ]
-  
 
   render() {
     return (
       <div className="App">
-        {this.state.map(data => {
-            return (
-              <div>
-                <p>Name: {data.name}</p>
-                <p>Age: {data.age}</p>
-              </div>
-            );
-          }
-        )}
+        <h1>HELLO WORLD!</h1>
       </div>
     );
   }
